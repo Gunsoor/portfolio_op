1b51697b1378bd3aa531babcd052b7b78679635f
Gunssor <gs951221@gmail.com>
2020-02-24 23:34:51 +0900

    - 스프링 시큐리티   - 계정정보 저장   - csrf   - 성공 핸들러   - 실패핸들러   - 계정 권한 - 로그백 - jdbc, mybatis

--- src/main/java/com/portfolio/op/controller/HomeController.java
+++ src/main/java/com/portfolio/op/controller/HomeController.java
@@ -1,14 +1,24 @@
 package com.portfolio.op.controller;
 
+import com.portfolio.op.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class HomeController {
-    @GetMapping("/home")
-    public String hoem(Model model){
+    @Autowired
+    UserService userService;
+
+    private Logger log = LoggerFactory.getLogger(this.getClass());
+
+    @GetMapping("/")
+    public String home(Model model){
+        log.debug("dndndndnd");
+        log.debug(userService.idCheck("gs951221"));
         model.addAttribute("test", "test");
         return "home";
     }
